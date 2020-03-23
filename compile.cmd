384fef8ea8a5b54aed5b1168e3e93c90838791f8
MooseOnTheRocks <sam.foltz@hotmail.com>
2020-03-22 19:36:16 -0700

    Updated compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,1 @@
-call cl /EHsc /MD src/main/*.cpp /Fegame.exe /I ".\ext\include\SDL2" /link /LIBPATH:".\ext\lib\SDL2\x86" SDL2.lib SDL2main.lib /SUBSYSTEM:CONSOLE
+call cl /EHsc /MD src/main/*.cpp /Feterm.exe /I ".\ext\include\SDL2" /link /LIBPATH:".\ext\lib\SDL2\x86" SDL2.lib SDL2main.lib /SUBSYSTEM:CONSOLE
